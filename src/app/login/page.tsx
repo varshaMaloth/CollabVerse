062c682b528ed222230bb7c774a9a74e616b4a72
Keerthana R <rkeerthana717@gmail.com>
2025-10-25 21:20:36 +0000

    remove that pic and collabverse on that pic too

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,22 +24,7 @@ export default function LoginPage() {
       <main className="flex-1">
         <div className="container relative h-full flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
           <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-            <div
-              className="absolute inset-0 bg-cover"
-              style={{
-                backgroundImage:
-                  "url('https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxtb3VudGFpbnxlbnwwfHx8fDE3MjE4MzE5ODJ8MA&ixlib=rb-4.1.0&w=1080&q=80')",
-                backgroundPosition: 'center',
-                filter: 'grayscale(100%) brightness(50%)',
-              }}
-            />
-            <div className="relative z-20 flex items-center text-lg font-medium">
-              <Briefcase className="mr-2 h-6 w-6" />
-              CollabVerse
-            </div>
-            <div className="relative z-20 mt-auto">
-              
-            </div>
+            <div className="relative z-20 mt-auto"></div>
           </div>
           <div className="lg:p-8 flex items-center justify-center">
             <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[450px]">
